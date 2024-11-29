2d4729960c005374628a7912353ac4da3c7f723a
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-11-29 10:40:25 +0000

    aa

--- graficos/common.js
+++ graficos/common.js
@@ -1,12 +1,12 @@
-const GETcss = (variavel) => {
+const getCSS = (variavel) => {
     const bodyStyles = getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
 }
 
 const tickConfig = {
     family: getCSS('--font'),
-    size:16,
+    size: 16,
     color: getCSS('--primary-color')
 }
 
-export {getCSS,tickConfig}
+export {getCSS, tickConfig}
